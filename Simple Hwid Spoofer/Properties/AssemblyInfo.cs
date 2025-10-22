0c80adbb7d51f804cc9b604e6c3b3b9aaa4c134c
Denis T. <deadspell21391@gmail.com>
2025-10-22 22:13:06 +0200

    Upload Files

---
+++ Simple Hwid Spoofer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Simple Hwid Spoofer")]
+[assembly: AssemblyDescription("Hwid Spoofer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple Hwid Spoofer")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("DEE70D0D-3FE9-42BB-AD45-5ED434C8C2DC")]
+[assembly: AssemblyVersion("6.86.996.2841")]
+[assembly: AssemblyFileVersion("6.86.996.2841")]
